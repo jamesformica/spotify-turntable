abf766b94d798a3273948585b42cf7accac2457c
James Formica <james.formica@gmail.com>
2019-03-20 07:24:10 +1100

    refactor: extract out helper

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,39 +1,17 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 
 import FateDispatch from '../context'
+import decideFate from '../helpers/fateDecider'
 import styles from './Input.css'
-
-const isAlive = (name) => {
-  const chars = name.split('')
-
-  const charTotal = chars.reduce((agg, curr) => {
-    const total = agg + curr.charCodeAt()
-    return total
-  }, 0)
-
-  return charTotal % 2 === 0
-}
-
-const submit = (e, name, dispatch) => {
-  e.preventDefault()
-
-  const victim = name.trim()
-
-  if (!victim) return null
-
-  return isAlive(victim)
-    ? dispatch({ type: 'ALIVE' })
-    : dispatch({ type: 'DEAD' })
-}
 
 const Input = () => {
   const inputEl = useRef()
-  const [name, setName] = useState('')
   const dispatch = useContext(FateDispatch)
+  const [name, setName] = useState('')
 
   const onInput = e => setName(e.target.value)
   const onChange = e => setName(e.target.value)
-  const onSubmit = e => submit(e, name, dispatch)
+  const onSubmit = e => decideFate(e, name, dispatch)
 
   useEffect(() => {
     inputEl.current.focus()
